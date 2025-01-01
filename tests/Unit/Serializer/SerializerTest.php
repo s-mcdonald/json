56674463adabb8c612fc76a63cbd4ff77b1240be
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-01 14:17:23 +1000

    update test

--- tests/Unit/Serializer/SerializerTest.php
+++ tests/Unit/Serializer/SerializerTest.php
@@ -6,16 +6,40 @@ namespace SamMcDonald\Json\Tests\Unit\Serializer;
 
 use PHPUnit\Framework\TestCase;
 use SamMcDonald\Json\Json;
+use SamMcDonald\Json\Tests\Unit\Serializer\Fixtures\BadPropertyNamesSerializable;
 use SamMcDonald\Json\Tests\Unit\Serializer\Fixtures\ParentClassSerializable;
 
 class SerializerTest extends TestCase
 {
-    public function testSerialize(): void
+    public function testSerializeWithUninitializedValues(): void
     {
         $sut = new ParentClassSerializable();
 
         static::assertEquals(
             '{}',
+            Json::serialize($sut),
+        );
+    }
+
+    public function testSerialize(): void
+    {
+        $sut = new ParentClassSerializable();
+        $sut->name = 'foo';
+        $sut->phoneNumbers = ['1234', '5678'];
+
+        static::assertEquals(
+            '{"userName":"foo","phoneNumbers":["1234","5678"]}',
+            Json::serialize($sut),
+        );
+    }
+
+    public function testSerializeWithBadPropertyNames(): void
+    {
+        $sut = new BadPropertyNamesSerializable();
+        $sut->name = 'foo';
+
+        static::assertEquals(
+            '{"user Name":"foo"}',
             Json::serialize($sut),
         );
     }
