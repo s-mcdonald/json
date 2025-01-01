8fb3db926badf59540e19db5ff2572f5740234c0
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-01 12:33:12 +1000

    update EncoderInterface to allow for formatting

--- src/Serializer/Encoding/Contracts/EncoderInterface.php
+++ src/Serializer/Encoding/Contracts/EncoderInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Encoding\Contracts;
 
+use SamMcDonald\Json\Serializer\Enums\JsonFormat;
+
 interface EncoderInterface
 {
-    public function encode($value): EncodingResultInterface;
+    public function encode($value, JsonFormat $format = JsonFormat::Pretty): EncodingResultInterface;
 }
