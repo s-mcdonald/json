cf268d0b2e76d1d0207f1173c3f52fd0fa0ef082
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-01 15:32:12 +1000

    added: Fixture GoodChildObjectSerializable

---
+++ tests/Unit/Serializer/Fixtures/GoodChildObjectSerializable.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Json\Tests\Unit\Serializer\Fixtures;
+
+use SamMcDonald\Json\Serializer\Attributes\JsonProperty;
+use SamMcDonald\Json\Serializer\Contracts\JsonSerializable;
+
+class GoodChildObjectSerializable implements JsonSerializable
+{
+    public function __construct(
+        #[JsonProperty('childProp1')]
+        private string $childProperty1,
+    ){
+    }
+
+    #[JsonProperty('childProp2')]
+    public string $childProperty2;
+}
