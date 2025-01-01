440d8b499951b8e03516e878ac044616bf4abe86
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-01 12:29:21 +1000

    implement initial struct for serializing Properties

--- src/Serializer/JsonSerializer.php
+++ src/Serializer/JsonSerializer.php
@@ -4,12 +4,36 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer;
 
+use ReflectionObject;
+use SamMcDonald\Json\Serializer\Encoding\JsonEncoder;
+use SamMcDonald\Json\Serializer\Encoding\Validator\JsonValidator;
+use SamMcDonald\Json\Serializer\Enums\JsonFormat;
+use stdClass;
+
 class JsonSerializer
 {
-    private const EMPTY = '';
+    private int $jsonSerializeFlags = 0;
 
-    public function serialize(Contracts\JsonSerializable $object, Enums\JsonFormat $format): string
+    public function serialize(Contracts\JsonSerializable $object, JsonFormat $format): string
     {
-        return self::EMPTY;
+        $classObject = new stdClass();
+        $reflectionScope = new ReflectionObject($object);
+
+        $this->serializeProperties($reflectionScope, $object, $classObject);
+
+        if (JsonFormat::Pretty === $format) {
+            $this->jsonSerializeFlags |= JSON_PRETTY_PRINT;
+        }
+
+        $encoder = new JsonEncoder(new JsonValidator(), flags: $this->jsonSerializeFlags);
+
+        return $encoder->encode($classObject)->getBody();
+    }
+
+    private function serializeProperties(
+        ReflectionObject $reflectionScope,
+        Contracts\JsonSerializable $object,
+        stdClass $classObject,
+    ): void {
     }
 }
