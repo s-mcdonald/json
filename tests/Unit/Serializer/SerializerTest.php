c70887ddbcf00ece6c19bf14ca9b96d369a18588
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-01 11:35:34 +1000

    setup basic test with no action until implementation

--- tests/Unit/Serializer/SerializerTest.php
+++ tests/Unit/Serializer/SerializerTest.php
@@ -5,8 +5,18 @@ declare(strict_types=1);
 namespace SamMcDonald\Json\Tests\Unit\Serializer;
 
 use PHPUnit\Framework\TestCase;
+use SamMcDonald\Json\Json;
+use SamMcDonald\Json\Tests\Unit\Serializer\Fixtures\ParentClassSerializable;
 
 class SerializerTest extends TestCase
 {
+    public function testSerialize(): void
+    {
+        $sut = new ParentClassSerializable();
 
+        static::assertEquals(
+            '',
+            Json::serialize($sut),
+        );
+    }
 }
