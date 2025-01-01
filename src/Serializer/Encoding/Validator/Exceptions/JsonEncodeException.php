8b574a8144e117e1c8454705087afaa6f3123b54
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-01 12:23:19 +1000

    added: JsonEncodeException

---
+++ src/Serializer/Encoding/Validator/Exceptions/JsonEncodeException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Json\Serializer\Encoding\Validator\Exceptions;
+
+use RuntimeException;
+
+class JsonEncodeException extends RuntimeException
+{
+}
