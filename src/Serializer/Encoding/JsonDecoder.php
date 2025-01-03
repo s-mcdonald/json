42c8784352dce3db32822f773e430515533cf8e7
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-03 23:41:01 +1000

    feature: hydration (m1) basic property hydration of json

--- src/Serializer/Encoding/JsonDecoder.php
+++ src/Serializer/Encoding/JsonDecoder.php
@@ -4,18 +4,38 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Encoding;
 
-use stdClass;
+use Exception;
+use SamMcDonald\Json\Serializer\Encoding\Contracts\DecodingResultInterface;
+use SamMcDonald\Json\Serializer\Hydrator;
 
-readonly class JsonDecoder
+readonly class JsonDecoder implements Contracts\DecoderInterface
 {
     public function __construct(
+        private Hydrator $hydrator,
         private int $depth = 512,
     ) {
     }
 
-    // @todo: WIP
-    public function decode(string $jsonValue, string $fqClassName): stdClass
+    public function decode(string $jsonValue, string|null $fqClassName = null): DecodingResultInterface
     {
-        return json_decode($jsonValue, false, $this->depth, JSON_FORCE_OBJECT);
+        try {
+            $decodedData = json_decode($jsonValue, false, $this->depth, JSON_THROW_ON_ERROR);
+
+            if (null !== $fqClassName) {
+                $decodedData = $this->hydrator->hydrate($decodedData, $fqClassName);
+            }
+        } catch (Exception $e) {
+            return new JsonDecodingResult(
+                '',
+                $e->getMessage(),
+                false,
+            );
+        }
+
+        return new JsonDecodingResult(
+            $decodedData,
+            $fqClassName ?? '',
+            true,
+        );
     }
 }
