d31cf3a5550da843df67df5b4c37b6950cf8e1ad
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-03 17:09:40 +1000

    add empty ArrayNormalizer

--- src/Serializer/Normalizers/ArrayNormalizer.php
+++ src/Serializer/Normalizers/ArrayNormalizer.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Normalizers;
 
+/**
+ * Normalize complex types into simple arrays. Good ol' php.
+ */
 class ArrayNormalizer
 {
+    public function normalize(): array
+    {
+        return [];
+    }
 }
