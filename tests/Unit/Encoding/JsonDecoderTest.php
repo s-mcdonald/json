5127f8ca48e92a57e46fef6dd6b34298e297d142
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-05 15:25:17 +1000

    test: add basic decoder test

--- tests/Unit/Encoding/JsonDecoderTest.php
+++ tests/Unit/Encoding/JsonDecoderTest.php
@@ -5,8 +5,25 @@ declare(strict_types=1);
 namespace SamMcDonald\Json\Tests\Unit\Encoding;
 
 use PHPUnit\Framework\TestCase;
+use SamMcDonald\Json\Serializer\Encoding\JsonDecoder;
+use SamMcDonald\Json\Serializer\Encoding\JsonDecodingResult;
 
 class JsonDecoderTest extends TestCase
 {
+    public function testDecode(): void
+    {
+        $decodedData = (object) ["foo" => "bar"];
+        $expectation = new JsonDecodingResult(
+            $decodedData,
+            '',
+            true,
+        );
 
+        $sut = new JsonDecoder();
+
+        static::assertEquals(
+            $expectation,
+            $sut->decode('{"foo":"bar"}')
+        );
+    }
 }
