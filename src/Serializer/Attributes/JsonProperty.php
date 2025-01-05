eb75311aa13c905e9f816ac781d5d160b88da0e9
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-05 11:22:30 +1000

    add getHasValidName to JsonProperty.php

--- src/Serializer/Attributes/JsonProperty.php
+++ src/Serializer/Attributes/JsonProperty.php
@@ -14,4 +14,23 @@ class JsonProperty
         private bool $deserialize = false,
     ) {
     }
+
+    public static function getHasValidName(string $propertyName): bool
+    {
+        if (!preg_match('/^[a-zA-Z]{1}[a-zA-Z0-9-_]*$/', $propertyName)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getName(): string|null
+    {
+        return $this->name;
+    }
+
+    public function getDeserialize(): bool
+    {
+        return $this->deserialize;
+    }
 }
