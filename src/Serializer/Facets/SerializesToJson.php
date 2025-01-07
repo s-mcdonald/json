5a47aee183dd73d91cb6b3b4d447654caeab2a57
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-07 18:32:17 +1000

    added: SerializesWithMapping

--- src/Serializer/Facets/SerializesToJson.php
+++ src/Serializer/Facets/SerializesToJson.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Facets;
 
-use SamMcDonald\Json\Serializer\Attributes\AttributeReader\JsonPropertyReader;
-use SamMcDonald\Json\Serializer\Enums\JsonFormat;
-use SamMcDonald\Json\Serializer\JsonSerializer;
-use SamMcDonald\Json\Serializer\Normalization\Normalizers\ArrayNormalizer;
-use SamMcDonald\Json\Serializer\Normalization\Normalizers\EntityNormalizer;
-
 trait SerializesToJson
 {
+    use SerializesWithMapping;
+
     protected function serializeToJson(array|null $mapping = null): string
     {
         if (null === $mapping) {
@@ -26,18 +22,5 @@ trait SerializesToJson
         }
 
         return $this->_toJson($arrayToNormalize);
-    }
-
-    final protected function _toJson(array|null $mapping = null): string
-    {
-        if (null === $mapping) {
-            $serializer = new JsonSerializer(objectNormalizer: new EntityNormalizer(new JsonPropertyReader()));
-
-            return $serializer->serialize($this, JsonFormat::Pretty);
-        }
-
-        $serializer = new JsonSerializer(objectNormalizer: new ArrayNormalizer());
-
-        return $serializer->serialize($mapping, JsonFormat::Pretty);
     }
 }
