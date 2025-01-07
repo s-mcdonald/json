44ee4dcfc98567d1bfc6c1806c96e14f19ee4658
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-07 18:14:15 +1000

    update: ObjectNormalizer.php to use base classes

--- src/Serializer/Normalization/Normalizers/ObjectNormalizer.php
+++ src/Serializer/Normalization/Normalizers/ObjectNormalizer.php
@@ -4,50 +4,23 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Normalization\Normalizers;
 
-use InvalidArgumentException;
 use ReflectionAttribute;
-use ReflectionClass;
-use ReflectionEnum;
-use ReflectionException;
 use ReflectionMethod;
-use ReflectionObject;
 use ReflectionProperty;
 use SamMcDonald\Json\Builder\JsonBuilder;
-use SamMcDonald\Json\Serializer\Attributes\AttributeReader\JsonPropertyReader;
 use SamMcDonald\Json\Serializer\Attributes\JsonProperty;
 use SamMcDonald\Json\Serializer\Exceptions\JsonSerializableException;
 use SamMcDonald\Json\Serializer\Normalization\Normalizers\Context\Context;
 use SamMcDonald\Json\Serializer\Normalization\Normalizers\Context\ContextBuilder;
-use SamMcDonald\Json\Serializer\Normalization\Normalizers\Contracts\NormalizerInterface;
-use stdClass;
-use TypeError;
+use SamMcDonald\Json\Serializer\Normalization\Normalizers\Contracts\AbstractClassNormalizer;
 
 /**
  * Normalize from object to stdClass. This includes Serializable methods with the
  * use of Attributes.
  */
-final readonly class ObjectNormalizer implements NormalizerInterface
+final class ObjectNormalizer extends AbstractClassNormalizer
 {
-    public function __construct(
-        private JsonPropertyReader $propertyReader,
-    ) {
-    }
-
-    public function normalize(mixed $input): stdClass
-    {
-        if (false === is_object($input)) {
-            throw new InvalidArgumentException('input must be an object.');
-        }
-
-        $reflectionClass = new ReflectionClass($input);
-        if ($reflectionClass->isEnum()) {
-            return $this->normalizeEnum($input);
-        }
-
-        return $this->transferToJsonBuilder($input)->toStdClass();
-    }
-
-    private function transferToJsonBuilder(object $propertyValue): JsonBuilder
+    protected function transferToJsonBuilder(object $propertyValue): JsonBuilder
     {
         $jsonBuilder = new JsonBuilder();
         $contextBuilder = new ContextBuilder($this->propertyReader);
@@ -63,6 +36,22 @@ final readonly class ObjectNormalizer implements NormalizerInterface
         }
 
         return $jsonBuilder;
+    }
+
+    protected function mapArrayContents(array $array): array
+    {
+        $newArray = [];
+        foreach ($array as $value) {
+            $newArray[] = match (true) {
+                is_null($value) => null,
+                is_bool($value), is_scalar($value) => $value,
+                is_array($value) => $this->mapArrayContents($value),
+                is_object($value) => $this->transferToJsonBuilder($value),
+                default => throw new JsonSerializableException('Invalid type in array.'),
+            };
+        }
+
+        return $newArray;
     }
 
     private function processProperty(Context $context): void
@@ -123,115 +112,19 @@ final readonly class ObjectNormalizer implements NormalizerInterface
         $this->assignToStdClass($context->getPropertyName(), $propertyValue, $context->getClassObject());
     }
 
-    private function assignToStdClass($propertyName, $propertyValue, JsonBuilder $classObject): void
-    {
-        if (is_object($propertyValue)) {
-            $classObject->addProperty($propertyName, $this->transferToJsonBuilder($propertyValue));
-
-            return;
-        }
-
-        match (\gettype($propertyValue)) {
-            'array' => $classObject->addProperty($propertyName, $this->mapArrayContents($propertyValue)),
-            'NULL', 'boolean', 'string', 'integer', 'double' => $classObject->addProperty($propertyName, $propertyValue),
-            default => throw new JsonSerializableException('Invalid type.'),
-        };
-    }
-
-    /**
-     * @return array<ReflectionProperty>
-     */
-    private function getReflectionProperties(object $originalObject): array
-    {
-        return (new ReflectionObject($originalObject))->getProperties(
-            ReflectionProperty::IS_PUBLIC |
-            ReflectionProperty::IS_PROTECTED |
-            ReflectionProperty::IS_PRIVATE,
-        );
-    }
-
-    /**
-     * @return array<ReflectionMethod>
-     */
-    private function getReflectionMethods(object $originalObject): array
-    {
-        return (new ReflectionObject($originalObject))->getMethods(
-            ReflectionProperty::IS_PUBLIC |
-            ReflectionProperty::IS_PROTECTED |
-            ReflectionProperty::IS_PRIVATE,
-        );
-    }
-
     /**
      * @param array<ReflectionAttribute> $attributes
      */
     private function isSerializable($propertyValue, array $attributes): bool
     {
-        if (
-            (null === $propertyValue || is_scalar($propertyValue) || is_array($propertyValue))
-            && $this->propertyReader->hasJsonPropertyAttributes($attributes)
-        ) {
-            return true;
+        if (false === $this->propertyReader->hasJsonPropertyAttributes($attributes)) {
+            return false;
         }
 
-        if (
-            is_object($propertyValue)
-            && $this->propertyReader->hasJsonPropertyAttributes($attributes)
-        ) {
+        if ($this->canValueSerializable($propertyValue)) {
             return true;
         }
 
         return false;
-    }
-
-    private function getValueFromPropOrMethod(
-        ReflectionMethod|ReflectionProperty $reflection,
-        $originalObject,
-    ): mixed {
-        if (false === ($reflection instanceof ReflectionMethod)) {
-            return $reflection->getValue($reflection->isStatic() ? null : $originalObject);
-        }
-
-        try {
-            $propertyValue = $reflection->invoke($reflection->isStatic() ? null : $originalObject);
-        } catch (TypeError $t) {
-            throw new JsonSerializableException('Value has not been initialized.');
-        } catch (ReflectionException $e) {
-            throw new JsonSerializableException($e->getMessage());
-        }
-
-        return $propertyValue;
-    }
-
-    private function mapArrayContents(array $array): array
-    {
-        $newArray = [];
-        foreach ($array as $value) {
-            $newArray[] = match (true) {
-                is_null($value) => null,
-                is_bool($value), is_scalar($value) => $value,
-                is_array($value) => $this->mapArrayContents($value),
-                is_object($value) => $this->transferToJsonBuilder($value),
-                default => throw new JsonSerializableException('Invalid type in array.'),
-            };
-        }
-
-        return $newArray;
-    }
-
-    private function normalizeEnum(object $propertyValue): stdClass
-    {
-        $reflectionClass = new ReflectionClass($propertyValue);
-
-        $value = $propertyValue->name;
-        if ((new ReflectionEnum($propertyValue))->isBacked()) {
-            $value = $propertyValue->value;
-        }
-
-        $builder = new JsonBuilder();
-
-        return $builder
-            ->addProperty($reflectionClass->getShortName(), $value)
-            ->toStdClass();
     }
 }
