18b0c48fb05b6afdc851f79664e56b09c930ffa6
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-07 18:14:03 +1000

    update: EntityNormalizer.php to use base classes

--- src/Serializer/Normalization/Normalizers/EntityNormalizer.php
+++ src/Serializer/Normalization/Normalizers/EntityNormalizer.php
@@ -4,49 +4,21 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Normalization\Normalizers;
 
-use InvalidArgumentException;
-use ReflectionClass;
-use ReflectionEnum;
-use ReflectionException;
-use ReflectionMethod;
-use ReflectionObject;
 use ReflectionProperty;
 use SamMcDonald\Json\Builder\JsonBuilder;
-use SamMcDonald\Json\Serializer\Attributes\AttributeReader\JsonPropertyReader;
 use SamMcDonald\Json\Serializer\Exceptions\JsonSerializableException;
 use SamMcDonald\Json\Serializer\Normalization\Normalizers\Context\Context;
 use SamMcDonald\Json\Serializer\Normalization\Normalizers\Context\ContextBuilder;
-use SamMcDonald\Json\Serializer\Normalization\Normalizers\Contracts\NormalizerInterface;
-use stdClass;
-use TypeError;
+use SamMcDonald\Json\Serializer\Normalization\Normalizers\Contracts\AbstractClassNormalizer;
 
 /**
  * Normalize object properties to stdClass. Similar to ObjectNormalizer however
  * this simply takes all properties of an object and does
  * not deal with Attributes or methods.
  */
-final readonly class EntityNormalizer implements NormalizerInterface
+final class EntityNormalizer extends AbstractClassNormalizer
 {
-    public function __construct(
-        private JsonPropertyReader $propertyReader,
-    ) {
-    }
-
-    public function normalize(mixed $input): stdClass
-    {
-        if (false === is_object($input)) {
-            throw new InvalidArgumentException('input must be an object.');
-        }
-
-        $reflectionClass = new ReflectionClass($input);
-        if ($reflectionClass->isEnum()) {
-            return $this->normalizeEnum($input);
-        }
-
-        return $this->transferToJsonBuilder($input)->toStdClass();
-    }
-
-    private function transferToJsonBuilder(object $propertyValue): JsonBuilder
+    protected function transferToJsonBuilder(object $propertyValue): JsonBuilder
     {
         $jsonBuilder = new JsonBuilder();
         $contextBuilder = new ContextBuilder($this->propertyReader);
@@ -59,76 +31,7 @@ final readonly class EntityNormalizer implements NormalizerInterface
         return $jsonBuilder;
     }
 
-    private function processProperty(Context $context): void
-    {
-        if (false === $context->getReflectionItem()->isInitialized($context->getOriginalObject())) {
-            return;
-        }
-
-        $propertyValue = $this->getValueFromPropOrMethod($context->getReflectionItem(), $context->getOriginalObject());
-
-        if (false === $this->isSerializable($propertyValue)) {
-            return;
-        }
-
-        $this->assignToStdClass($context->getPropertyName(), $propertyValue, $context->getClassObject());
-    }
-
-    private function isSerializable($propertyValue): bool
-    {
-        return null === $propertyValue
-            || is_scalar($propertyValue)
-            || is_array($propertyValue)
-            || is_object($propertyValue);
-    }
-
-    private function assignToStdClass($propertyName, $propertyValue, JsonBuilder $classObject): void
-    {
-        if (is_object($propertyValue)) {
-            $classObject->addProperty($propertyName, $this->transferToJsonBuilder($propertyValue));
-
-            return;
-        }
-
-        match (\gettype($propertyValue)) {
-            'array' => $classObject->addProperty($propertyName, $this->mapArrayContents($propertyValue)),
-            'NULL', 'boolean', 'string', 'integer', 'double' => $classObject->addProperty($propertyName, $propertyValue),
-            default => throw new JsonSerializableException('Invalid type.'),
-        };
-    }
-
-    /**
-     * @return array<ReflectionProperty>
-     */
-    private function getReflectionProperties(object $originalObject): array
-    {
-        return (new ReflectionObject($originalObject))->getProperties(
-            ReflectionProperty::IS_PUBLIC |
-            ReflectionProperty::IS_PROTECTED |
-            ReflectionProperty::IS_PRIVATE,
-        );
-    }
-
-    private function getValueFromPropOrMethod(
-        ReflectionMethod|ReflectionProperty $reflection,
-        $originalObject,
-    ): mixed {
-        if (false === ($reflection instanceof ReflectionMethod)) {
-            return $reflection->getValue($reflection->isStatic() ? null : $originalObject);
-        }
-
-        try {
-            $propertyValue = $reflection->invoke($reflection->isStatic() ? null : $originalObject);
-        } catch (TypeError $t) {
-            throw new JsonSerializableException('Value has not been initialized.');
-        } catch (ReflectionException $e) {
-            throw new JsonSerializableException($e->getMessage());
-        }
-
-        return $propertyValue;
-    }
-
-    private function mapArrayContents(array $array): array
+    protected function mapArrayContents(array $array): array
     {
         $newArray = [];
         foreach ($array as $value) {
@@ -144,19 +47,18 @@ final readonly class EntityNormalizer implements NormalizerInterface
         return $newArray;
     }
 
-    private function normalizeEnum(object $propertyValue): stdClass
+    private function processProperty(Context $context): void
     {
-        $reflectionClass = new ReflectionClass($propertyValue);
-
-        $value = $propertyValue->name;
-        if ((new ReflectionEnum($propertyValue))->isBacked()) {
-            $value = $propertyValue->value;
+        if (false === $context->getReflectionItem()->isInitialized($context->getOriginalObject())) {
+            return;
         }
 
-        $builder = new JsonBuilder();
+        $propertyValue = $this->getValueFromPropOrMethod($context->getReflectionItem(), $context->getOriginalObject());
 
-        return $builder
-            ->addProperty($reflectionClass->getShortName(), $value)
-            ->toStdClass();
+        if (false === $this->canValueSerializable($propertyValue)) {
+            return;
+        }
+
+        $this->assignToStdClass($context->getPropertyName(), $propertyValue, $context->getClassObject());
     }
 }
