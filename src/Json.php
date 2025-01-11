c90154a7f58aff10e70c04872e3c8d36a5e5f83a
Sam McDonald <s.mcdonald@outlook.com.au>
2025-01-11 11:16:38 +1000

    update: Json, set static instances

--- src/Json.php
+++ src/Json.php
@@ -15,8 +15,41 @@ use SamMcDonald\Json\Serializer\Transformer\JsonUtilities;
 
 final class Json
 {
+    private static JsonUtilities|null $jsonUtilities = null;
+
+    private static JsonFormatter|null $jsonFormatter = null;
+
+    private static JsonSerializer|null $jsonSerializer = null;
+
     private function __construct(private string $json)
     {
+    }
+
+    private static function getJsonSerializer(): JsonSerializer
+    {
+        if (null === self::$jsonSerializer) {
+            self::$jsonSerializer = new JsonSerializer();
+        }
+
+        return self::$jsonSerializer;
+    }
+
+    private static function getJsonUtilities(): JsonUtilities
+    {
+        if (null === self::$jsonUtilities) {
+            self::$jsonUtilities = new JsonUtilities();
+        }
+
+        return self::$jsonUtilities;
+    }
+
+    private static function getJsonFormatter(): JsonFormatter
+    {
+        if (null === self::$jsonFormatter) {
+            self::$jsonFormatter = new JsonFormatter();
+        }
+
+        return self::$jsonFormatter;
     }
 
     public function toPretty(): string
@@ -40,12 +73,12 @@ final class Json
         object $object,
         JsonFormat $format = JsonFormat::Compressed,
     ): string {
-        return (new JsonSerializer())->serialize($object, $format);
+        return self::getJsonSerializer()->serialize($object, $format);
     }
 
     public static function deserialize(string $json, string $classFqn): mixed
     {
-        return (new JsonSerializer())->deserialize($json, $classFqn);
+        return self::getJsonSerializer()->deserialize($json, $classFqn);
     }
 
     public static function createJsonBuilder(): JsonBuilder
@@ -55,32 +88,32 @@ final class Json
 
     public static function prettify(string $json): string
     {
-        return (new JsonFormatter())->pretty($json);
+        return self::getJsonFormatter()->pretty($json);
     }
 
     public static function uglify(string $json): string
     {
-        return (new JsonFormatter())->ugly($json);
+        return self::getJsonFormatter()->ugly($json);
     }
 
     public static function isValid(string $json): bool
     {
-        return (new JsonUtilities())->isValid($json);
+        return self::getJsonUtilities()->isValid($json);
     }
 
     public static function push(string $json, string $key, mixed $item): string|false
     {
-        return (new JsonUtilities())->push($json, $key, $item);
+        return self::getJsonUtilities()->push($json, $key, $item);
     }
 
     public static function remove(string $json, string $property): string|false
     {
-        return (new JsonUtilities())->remove($json, $property);
+        return self::getJsonUtilities()->remove($json, $property);
     }
 
     public static function toArray(string $json): array|false
     {
-        return (new JsonUtilities())->toArray($json);
+        return self::getJsonUtilities()->toArray($json);
     }
 
     public static function iterate(string $json): ArrayIterator
